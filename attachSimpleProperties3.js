e9a1e5d074f5c42cf2b642907751ace2b2f77252
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2022-12-28 12:21:54 +0530

    different approach

--- attachSimpleProperties3.js
+++ attachSimpleProperties3.js
@@ -2,6 +2,33 @@
 // you may make modifications to objectCreator1 if required
 // invoke objectCreator1 inside objectCreator2 in such a way that property1 to property5 are attached to the obj in objectCreator2
 
+// function objectCreator1(o, a, b, c, d, e) {
+//   let obj = o;
+
+//   obj.property1 = a;
+//   obj.property2 = b;
+//   obj.property3 = c;
+//   obj.property4 = d;
+//   obj.property5 = e;
+
+//   return obj;
+// }
+
+// function objectCreator2(a, b, c, d, e, f) {
+//   let obj = {};
+
+//   let obj2 = {};
+//   obj2 = objectCreator1(obj2, a, b, c, d, e);
+//   obj = { ...obj2 };
+//   obj.property6 = f;
+
+//   return obj;
+// }
+
+// console.log(objectCreator2("a", "b", "c", "d", "e", "f"));
+
+//Different approach without spread bewlow
+
 function objectCreator1(o, a, b, c, d, e) {
   let obj = o;
 
@@ -17,9 +44,8 @@ function objectCreator1(o, a, b, c, d, e) {
 function objectCreator2(a, b, c, d, e, f) {
   let obj = {};
 
-  let obj2 = {};
-  obj2 = objectCreator1(obj2, a, b, c, d, e);
-  obj = { ...obj2 };
+  obj = objectCreator1(obj, a, b, c, d, e);
+
   obj.property6 = f;
 
   return obj;
