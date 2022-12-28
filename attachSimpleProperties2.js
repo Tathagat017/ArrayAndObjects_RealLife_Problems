5e00be02421a9fb868df2dc39314acafe0bce264
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2022-12-28 12:30:05 +0530

    New approches to optimize and avoid accidental referenced changes in non primitive

--- attachSimpleProperties2.js
+++ attachSimpleProperties2.js
@@ -28,3 +28,23 @@ let y = objectCreator2();
 y.property1 = 5;
 console.log(objectCreator2().property1);
 console.log(x.property1, y.property1);
+
+//Different approach without spread //shallow copy
+// function objectCreator1(obj = {}) {
+//   obj.property1 = 1;
+//   obj.property2 = 2;
+//   obj.property3 = 3;
+//   obj.property4 = 4;
+//   obj.property5 = 5;
+
+//   return obj;
+// }
+
+// function objectCreator2() {
+//   let obj = {};
+//   obj = objectCreator1();
+//   obj.property6 = 6;
+//   return obj;
+// }
+// console.log(objectCreator2());
+// console.log(objectCreator1());
